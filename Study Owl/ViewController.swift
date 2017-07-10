3deae3be3b6d2fbc08a9adda40c9fab6e15bfd0e
Alexandra Stavrakos <lexistavrakos@Alexandras-MacBook-Pro.local>
2017-07-10 13:41:10 -0500

    constraints

--- Study Owl/ViewController.swift
+++ Study Owl/ViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var visualView: UIView!
+    @IBOutlet weak var auditoryView: UIView!
+    @IBOutlet weak var kinestheticView: UIView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    var classes = ["Science", "Math", "Reading/Writing", "History"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.isHidden = true
+        pickerView.accessibilityElementsHidden = true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return classes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return classes[row]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
