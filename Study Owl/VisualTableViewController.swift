5711e73a61124334af3e53e65455d63f7a05430f
Brian Browdy <BrianBrowdy@Brians-MacBook-Air.local>
2017-07-10 17:33:16 -0500

    created first two cells for visual math resources

--- Study Owl/VisualTableViewController.swift
+++ Study Owl/VisualTableViewController.swift
@@ -11,27 +11,21 @@ import SafariServices
 class VisualTableViewController: UITableViewController {
 
     let math = ["http://www.commoncoresheets.com/Operations.php", "http://www.coolmath.com/prealgebra/05-order-of-operations/05-order-of-operations-parenthesis-PEMDAS-04"]
+    let mathTitles = ["Order of Operations Worksheet", "Order of Operations Picture Lesson"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mathTitles.count
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-      return 0
+    
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = mathTitles[indexPath.row]
+        return cell
         
     }
-
 }
