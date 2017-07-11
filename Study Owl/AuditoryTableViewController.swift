918cfc5673196e9d094a009c86a176c2f0e86682
Alexandra Stavrakos <lexistavrakos@dhcp-10-105-125-124.wireless.northwestern.private>
2017-07-11 17:18:43 -0500

    Added data to auditory

--- Study Owl/AuditoryTableViewController.swift
+++ Study Owl/AuditoryTableViewController.swift
@@ -12,12 +12,16 @@ class AuditoryTableViewController: UITableViewController {
     
     var myClass = OwlClass()
     
-    let math = ["https://www.khanacademy.org/math/cc-sixth-grade-math/cc-6th-ratios-prop-topic",]
-    let mathTitles = ["Ratios, rates, percentages"]
-    let science = ["https://google.com"]
-    let scienceTitles = [""]
-    let english = ["https://quizlet.com"]
-    let englishTitles = ["a"]
+    let math = ["https://www.khanacademy.org/math/cc-sixth-grade-math/cc-6th-ratios-prop-topic", "https://www.woboe.org/Page/6178", "https://www.khanacademy.org/math/cc-sixth-grade-math/cc-6th-arithmetic-operations"]
+    let mathTitles = ["Ratios, rates, percentages", "Videos for fractions and decimals", "Khan - Arithmetic operations videos"]
+    
+    let science = ["https://www.youtube.com/watch?v=LEHR8YQNm_Q&list=PLevTym1mJHsTllKYwJdhqgd4bjoZ_wlUZ", "https://www.youtube.com/playlist?list=PLRSIe0MUxDndmkA4PyU-Zw6mNKVVcIXE8", "https://www.youtube.com/playlist?list=PLRSIe0MUxDne1y4uS2w0H7oIH7w_ZZGts", "https://www.youtube.com/playlist?list=PLRSIe0MUxDncRjJ2vYvsXDU0pDnKObrAD", "https://quizlet.com/167626385/scientific-method-flash-cards/" ]
+    let scienceTitles = ["YouTube Playlist - Basic Physics & Biology", "Basic Physics Playlist", "Basic Biology Playlist", "Basic Chemistry", "Quizlet - Scientific Method"]
+    
+    
+    let english = ["http://www.corestandards.org/ELA-Literacy/W/6/", "https://quizlet.com/120732897/6th-grade-common-core-vocabulary-flash-cards/"]
+    let englishTitles = ["Common Core Standards", "Vocabulary and Concepts"]
+    
     let socialStudies = ["https://bing.com"]
     let socialStudiesTitles = [""]
     
